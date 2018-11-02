69505dc251998a28b04f8306ee0ca4a37c13cdc9
Johnatan Pereira da Silva <42559146+johpi@users.noreply.github.com>
2018-11-02 19:28:22 -0300

    Update 4.php

--- produtos/4.php
+++ produtos/4.php
@@ -96,8 +96,8 @@ and open the template in the editor.
                             <a style="cursor:zoom-in;" href="https://mariafilo.vteximg.com.br/arquivos/ids/212209-500-650/04013048_019_2-BLUSA-MANGA-TRIGUEIRO.jpg?v=636718539106730000" target="_blank"><img class="" src="https://mariafilo.vteximg.com.br/arquivos/ids/212209-500-650/04013048_019_2-BLUSA-MANGA-TRIGUEIRO.jpg?v=636718539106730000" width="auto" height="500"/></a>
                         </div>
                         <div class="fleft w30">
-                            <h3>R$ 249,00</h3> 
-                            <p>até 2x de R$ 124,50</p>
+                            <h3>R$ 299,00</h3> 
+                            <p>até 2x de R$ 149,50</p>
                             <p>Vendido e Enviado por StarClothes</p>
                             <br>
                             <p><b>Cor:</b>  Estampado</p>
