42cda306f430e7a638b4880fcf124338be974716
Johnatan Pereira da Silva <42559146+johpi@users.noreply.github.com>
2018-11-02 19:27:27 -0300

    Update 3.php

--- produtos/3.php
+++ produtos/3.php
@@ -97,8 +97,8 @@ and open the template in the editor.
                             <a style="cursor:zoom-in;" href="https://mariafilo.vteximg.com.br/arquivos/ids/211879-500-650/04061822_019_2-VESTIDO-MIDI-JATOBA.jpg?v=636715629161170000" target="_blank"><img class="" src="https://mariafilo.vteximg.com.br/arquivos/ids/211879-500-650/04061822_019_2-VESTIDO-MIDI-JATOBA.jpg?v=636715629161170000" width="auto" height="500"/></a>
                         </div>
                         <div class="fleft w30">
-                            <h3>R$ 249,00</h3> 
-                            <p>até 2x de R$ 124,50</p>
+                            <h3>R$ 499,00</h3> 
+                            <p>até 4x de R$ 124,75</p>
                             <p>Vendido e Enviado por StarClothes</p>
                             <br>
                             <p><b>Cor:</b>  Estampado</p>
